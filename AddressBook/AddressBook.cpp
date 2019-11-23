f252b7f127c5b2e7d5e7e3cf40b717cdb6f5275d
lenjoyn <lenjoyn@hotmail.com>
2019-11-23 15:15:33 +0000

    address book

--- AddressBook/AddressBook.cpp
+++ AddressBook/AddressBook.cpp
@@ -1,9 +1,104 @@
 #include "AddressBook.h"
+#include "iostream"
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
-void AddressBook::addFirst(string s){
+void AddressBook::add(std::string first){
   
-  	if(FirstMap[s]!=null){
+  	if(FirstMap[first]!=NULL) {
+		std::cout<<"entry exists, try another name\n";
+		return; 
+        }
         
+        FirstMap[first] = std::make_shared<Entry>(first); 
+}
 
-         
+
+void AddressBook::modifyOther(std::string first, std::string other){
+	if(FirstMap[first]==NULL){
+		std::cout<<"entry doesn't exist, please check the given first name\n";
+		return;
+	}
+
+	addOther(other, FirstMap[first]);
+}
+
+
+void AddressBook::modifyStreet(std::string first, std::string street){
+	if(FirstMap[first]==NULL){
+		std::cout<<"entry doesn't exist, please check the given first name\n";
+		return;
+	}
+
+	addStreet(street, FirstMap[first]);
+}
+void AddressBook::modifyTown(std::string first, std::string town){
+	if(FirstMap[first]==NULL){
+		std::cout<<"entry doesn't exist, please check the given first name\n";
+		return;
+	}
+
+	addTown(town, FirstMap[first]);
+}
+void AddressBook::modifyCountry(std::string first, std::string country){
+	if(FirstMap[first]==NULL){
+		std::cout<<"entry doesn't exist, please check the given first name\n";
+		return;
+	}
+
+	addCountry(country, FirstMap[first]);
+}
+
+void AddressBook::deleteFirst(std::string first){
+	if(FirstMap[first]==NULL){
+		std::cout<<"entry doesn't exist, so nothing will be performed\n";
+		return;
+	}
+
+	FirstMap[first]=NULL;
+}
+
+int main(int argc, char* argv[] ){ 
+
+
+   AddressBook adb;
+   std::string inFile = "record.txt";
+   std::ofstream of;
+   // read the file list, check the format is correct, and assign the numbers 
+   std::ifstream input(inFile.c_str());
+   if(!input) {
+	   std::cout<<"no record is found, creating a new file\n";
+	   of.open(inFile);
+   }
+   else{
+      std::string line;
+      while(getline(input, line)){
+	   std::istringstream iss(line);
+           std::string str;
+	   std::vector<std::string> cols;
+	   while(iss>>str) cols.push_back(str);
+
+           adb.add(cols[0]);
+	   adb.modifyOther(cols[0],cols[1]);
+	   adb.modifyTown(cols[0],cols[2]);
+	   adb.modifyCountry(cols[0],cols[3]);
+	   adb.modifyStreet(cols[0],cols[4]);
+      }
+   }
+
+/*   std::string command;
+   cin>>command;
+   while(command!="exit"){
+	std::cout>>"which operation??\n";
+
+	istringstream iss(command);
+	string operation, content;
+	iss>>operation>>" ">>content;
+
+	if(operation == "addFirstName") abd.add(content);
+	cin>>command;
+   }
+*/
+   return 0;
 }
--- AddressBook/AddressBook.h
+++ AddressBook/AddressBook.h
@@ -1,92 +1,166 @@
+#ifndef ADDRESSBOOK_H
+#define ADDRESSBOOK_H
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include <vector>
+#include <algorithm>
 class AddressBook{
 
 private:
 	class Entry{
  
         private:
-		string Firts;
-		string Other;
-		string Email;
-		string Phone;
-		string Town;
-		string Country;
-		string Street;
-		string Address;
- 
+		std::string First;
+		std::string Other;
+		std::string Country;
+		std::string Street;
+		std::string Town;
 
 
         public:
-	      void addFirst(string name){
-		      First = name;
+
+	      Entry(std::string name){
+	             First  = name;
+		     Other  = "NONE";
+		     Street = "NONE";
+		     Town   = "NONE";
+		     Country= "NONE";
               }
-	      void addOther(string name){
+	      void addOther(std::string name){
                       Other = name;
               }
-              void addMail(string mail){
-		      Email = mail;
-	      }
-	      void addPhone(string number){
-		      Phone = number;
-	      }
-	      void addStreet(string add){
+	      void addStreet(std::string add){
 		      Street = add;
 	      }
-	      void addTown(string add){
+	      void addTown(std::string add){
 		      Town = add;
               }
-	      void addCountry(string add){
+	      void addCountry(std::string add){
 		      Country = add;
 	      }
-	      
-	    
-        }
+
+	      //get methods for private members
+	      std::string getFirst(){
+		      return First;
+	      }
+	      std::string getOther(){
+		      return Other;
+	      }
+	      std::string getCountry(){
+		      return Country;
+	      }
+	      std::string getStreet(){
+		      return Street;
+	      }
+	      std::string getTown(){
+		      return Town;
+	      }
+        };
 
 	//unordered map to manage the addressbook
-	unordered_map<string, std::shared_ptr<Entry>>    FirstMap;
-	unordered_map<ap<string, std::shared_ptr<Entry>> OtherMap;
-	unordered_map<string, std::shared_ptr<Entry>>    MailMap;
-	unordered_map<string, std::shared_ptr<Entry>>    PhoneMap;
-	unordered_map<string, std::shared_ptr<Entry>>    StreetMap;
-	unordered_map<string, std::shared_ptr<Entry>>    TownMap;
-	unordered_map<string, std::shared_ptr<Entry>>    CountryMap;
+	std::unordered_map<std::string, std::shared_ptr<Entry>>    FirstMap;
+        
+	std::unordered_map<std::string, std::vector<std::string>> OtherMap;
+	std::unordered_map<std::string, std::vector<std::string>> StreetMap;
+	std::unordered_map<std::string, std::vector<std::string>> TownMap;
+	std::unordered_map<std::string, std::vector<std::string>> CountryMap;
 
-        void addFirst(string name, std::shared_ptr<Entry>>; 
-        void addOther(string name, std::shared_ptr<Entry>);
-        void addMail(string mail, std::shared_ptr<Entry>);
-        void addPhone(string phone,);
-        void addStreet(sring add);
-        void addTown(string add);
-	void addCountry(string add);
+        void addOther(std::string name, std::shared_ptr<Entry> entry){
+		
+		if(entry->getOther()==name ) return;
+		else if(entry->getOther()=="NONE") {
+			entry->addOther(name);
+			OtherMap[name].push_back(entry->getFirst());
+	        }
+		else{
+			std::string old = entry->getOther();
+			std::string first=entry->getFirst();
+			entry->addOther(name);
+			//a new value is given to other name, erase the old one from map
+			OtherMap[old].erase(std::remove_if(OtherMap[old].begin(),OtherMap[old].end(),
+						[&](std::string s)->bool{ return s==first;}),OtherMap[old].end());
+			OtherMap[name].push_back(first);
+		}
+		
+        }
+        void addStreet(std::string add, std::shared_ptr<Entry> entry){
+		
+		if(entry->getStreet()==add ) return;
+		else if(entry->getStreet()=="NONE") {
+			entry->addStreet(add);
+			StreetMap[add].push_back(entry->getFirst());
+	        }
+		else{
+			std::string old = entry->getStreet();
+			std::string first=entry->getFirst();
+			StreetMap[old].erase(std::remove_if(StreetMap[old].begin(), StreetMap[old].end(),
+						[&](std::string s)->bool{return s==first;}), StreetMap[old].end());
+
+			StreetMap[add].push_back(first);
+			entry->addStreet(add);
+		}
+        }
+        void addTown(std::string add, std::shared_ptr<Entry> entry){
+		
+		if(entry->getTown()==add ) return;
+		else if(entry->getTown()=="NONE") {
+			entry->addTown(add);
+			TownMap[add].push_back(entry->getFirst());
+	        }
+		else{
+			std::string old = entry->getTown();
+			std::string first=entry->getFirst();
+                        TownMap[old].erase(std::remove_if(TownMap[old].begin(), TownMap[old].end(),
+			                          [&](std::string s)->bool{return s==first;}), TownMap[old].end());
+
+			TownMap[add].push_back(first);
+			entry->addTown(add);
+		}
+	}
+	void addCountry(std::string add, std::shared_ptr<Entry> entry){
+		
+		if(entry->getCountry()==add ) return;
+		else if(entry->getCountry()=="NONE") {
+			entry->addCountry(add);
+			CountryMap[add].push_back(entry->getFirst());
+	        }
+		else{
+			std::string old = entry->getCountry();
+			std::string first=entry->getFirst();
+			CountryMap[old].erase(remove(CountryMap[old].begin(),CountryMap[old].end(), first),CountryMap[old].end());
+			CountryMap[old].erase(std::remove_if(CountryMap[old].begin(), CountryMap[old].end(),
+						[&](std::string s)->bool{return s==first;}), CountryMap[old].end());
+			CountryMap[add].push_back(first);
+			entry->addCountry(add);
+		}
+        }
+
+	std::string getAddress(std::shared_ptr<Entry> entry){
+		return entry->getStreet()+" "+entry->getTown()+" "+entry->getCountry();
+	}
 	
 public:
         //methods to modify addressbook
-	//add entry
-	void addFirst(string name); 
-        void addOther(string name);
-        void addMail(string mail);
-        void addPhone(string phone);
-        void addStreet(sring add);
-        void addTown(string add);
-	void addCountry(string add);
-        //search entry
-	Entry* searchFirts(string name);
-	Entry* searchOther(string name);
-	Entry* searchMail(string mail);
-	Entry* searchPhone(string phone);
-	Entry* searchTown(string add);
-	Entry* searchCountry(string add);
+	//add entry only available by firstname
+	void add(std::string name); 
 
-	void searchGeneral(string s);
-       
-	
-	void deleteFirst(string name); 
-        void deleteOther(string name);
-        void deleteMail(string mail);
-        void deletePhone(string phone);
-        void deleteStreet(sring add);
-        void deleteTown(string add);
-	void deleteCountry(string add);
-
+	//other parameters could only be modified if firstname exists
+        void modifyOther(std::string first, std::string other);
+        void modifyStreet(std::string first, std::string add);
+        void modifyTown(std::string first, std::string add);
+	void modifyCountry(std::string first, std::string add);
+/*
+	//search entry
+	std::shared_ptr<Entry> searchFirts(std::string name){return FirstMap[name];}
+	std::vector<std::shared_ptr<Entry>> searchOther(std::string name){return OtherMap[name];}
+	std::vector<std::shared_ptr<Entry>> searchTown(std::string add)  {return StreetMap[name];}
+	std::vector<std::shared_ptr<Entry>> searchCountry(std::string add){return CountryMap[name];};
+*/
+	//delete is only available by first name 
+	void deleteFirst(std::string name); 
+/*
 	void OutWriet();   //method to output the record
-	void InLoad(string txt);  //method to load the existing record
-}
+	void InLoad(std::string txt);  //method to load the existing record
+*/};
+#endif
